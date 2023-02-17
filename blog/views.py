8adf1fbcf130cd2bf3d94a43a98dcebef77138da
Wendel Segadilha <wendelsegadilha99@gmail.com>
2023-02-17 11:09:49 -0300

    finalizado cap 02

--- blog/views.py
+++ blog/views.py
@@ -1,35 +1,72 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from taggit.models import Tag
+from django.db.models import Count
 
 # Create your views here.
 
-def post_list(request):
+
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
-    paginator = Paginator(object_list, 3) # 3 posts in each page
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
+    paginator = Paginator(object_list, 3)  # 3 posts in each page
     page = request.GET.get('page')
     try:
         posts = paginator.page(page)
     except PageNotAnInteger:
-    # If page is not an integer deliver the first page
+        # If page is not an integer deliver the first page
         posts = paginator.page(1)
     except EmptyPage:
-    # If page is out of range deliver last page of results
+        # If page is out of range deliver last page of results
         posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
+    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts, 'tag':tag})
+
 
 def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month, publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
+    post = get_object_or_404(Post, slug=post, status='published',
+                             publish__year=year, publish__month=month, publish__day=day)
+
+    # lista de comentários ativos
+    comments = post.comments.filter(active=True)
+
+    new_comment = None
+
+    if request.method == 'POST':
+        # cria formulário com os dados do POST
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # crio um comentário sem salvar no banco
+            new_comment = comment_form.save(commit=False)
+            # relaciona post e comentário
+            new_comment.post = post
+            # salvo o comentário no banco
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    
+    #lista de posts com tags semelhantes
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
+    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'new_comment': new_comment, 'comment_form': comment_form, 'similar_posts': similar_posts})
+
 
 class PostListView(ListView):
- queryset = Post.published.all()
- context_object_name = 'posts'
- paginate_by = 3
- template_name = 'blog/post/list.html'
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
 
 def post_share(request, post_id):
     # Retrieve post by id
@@ -51,5 +88,4 @@ def post_share(request, post_id):
 
     else:
         form = EmailPostForm()
-    return render(request, 'blog/post/share.html', {'post': post,'form': form,'sent': sent})
-
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
